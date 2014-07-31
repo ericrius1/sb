bf2490fd60df9d7e004d20891e75b0ce21435799
Eric <Ericrius1@gmail.com>
2014-07-31 17:52:30 +0200

    hall of friend

--- js/lights.js
+++ js/lights.js
@@ -1,6 +1,9 @@
 var Lights = function (){
   //color, intensity, distance
-  var color = 0xffffff;
+
+  var ambientLight = new THREE.AmbientLight(0xffffff);
+  // scene.add(ambientLight);
+  var color = 0xff00ff;
   var intensity = 2.5;
   var pointLight = new THREE.PointLight(color,1);
   var pointMesh = new THREE.Mesh(new THREE.SphereGeometry(2,2));
